b87c3e22e484aac4870e79091f6e95e1189db9e7
DESKTOP-LDLH62C\黃虹妹 <mjmj3794@gmail.com>
2023-12-10 21:51:32 +0800

    前台畫面購物車清單

--- js/all.js
+++ js/all.js
@@ -1,9 +1,11 @@
 let productData=[];
-let productWrap=document.querySelector('.productWrap');
+let productList=document.querySelector('.productWrap');
+
 
 //初始化fun
 function init(){
     getProductList();
+    getCartList();
 }
 init();
 
@@ -26,7 +28,7 @@ function combineProductHTMLItem(item){
     return `<li class="productCard">
                 <h4 class="productType">新品</h4>
                 <img src="${item.images}">
-                <a href="#" class="addCardBtn" data-id='${item.id}'>加入購物車</a>
+                <a href="#" id="addCardBtn" class='js-addCart' data-id='${item.id}'>加入購物車</a>
                 <h3>${item.title}</h3>
                 <del class="originPrice">NT$${item.origin_price}</del>
                 <p class="nowPrice">NT$${item.price}</p>
@@ -40,7 +42,7 @@ function renderProductList(){
         //渲染因為有分別不同功能，所以會切換不同的頁面，所以將內容另外寫
         str+= combineProductHTMLItem(item);
     })
-    productWrap.innerHTML=str;
+    productList.innerHTML=str;
 }
 
 //console.log(productData);
@@ -64,12 +66,113 @@ productSelect.addEventListener('change',function(e){
     productWrap.innerHTML=str;
 })
 
-const addCardBtn=document.querySelector('.addCardBtn');
-productWrap.addEventListener('click',function(e){
+//const addCardBtn=document.querySelector('.addCardBtn');
+//從外層綁監聽 就不用一顆一顆綁
+productList.addEventListener('click',function(e){
     //回覆他的初始值，因為點加入購物車有預設 a #
     e.preventDefault();
-    console.log(e.target.getAttribute('data-id'));
-    productData.forEach(function(item,index){
-
-    });
+    // console.log(e.target.getAttribute('data-id'));
+    // productData.forEach(function(item,index){
+    //     //if()
+    // });
+    let addCartClass=e.target.getAttribute('class');
+    if(addCartClass !== "js-addCart"){
+        return;
+    }
+    let productID=e.target.getAttribute('data-id');//紀錄產品的id
+    console.log(productID);
+    //加入購物車功能
+    let numCheck =1; //預設一筆資料
+    cartData.forEach(function(item){
+        if(item.product.id===productID){//雌料庫的id與紀錄產品的id是否相同
+            //有一樣的品項
+            numCheck=item.quantity+=1;
+        }//沒有的話就傳1給他
+        //post
+    })
+    //console.log(numCheck); 確認典籍按鈕有新增數量到指定產品
+    axios.post(`${url}/api/livejs/v1/customer/${api_path}/carts`,{
+            "data": {
+              "productId":productID,
+              "quantity": numCheck
+            }
+    })
+    .then(function(response){
+        console.log(response);
+        getCartList();
+        alert('加入購物車');
+    })
 });
+
+let cartData=[];
+const cartList = document.querySelector('.shoppingCart-tableList');//公用變數
+//顯示購物車清單
+function getCartList(){
+    axios.get(`${url}/api/livejs/v1/customer/${api_path}/carts`)
+    .then(function(response){
+        cartData=response.data.carts;
+        //可以新增一個如果購物車無資料可顯示甚麼<可以依照資料長度=0時
+        let str='';
+        console.log(response.data.finalTotal);
+        //總計核算的寫法，如果後端有寫就用後端的
+        document.querySelector('.js-total').textContent=response.data.finalTotal
+        cartData.forEach(function(item){   
+            str+=`<tr>
+                    <td>
+                        <div class="cardItem-title">
+                            <img src="${item.product.images}" alt="">
+                            <p>${item.product.title}</p>
+                        </div>
+                    </td>
+                    <td>NT$${item.product.price}</td>
+                    <td>${item.quantity}</td>
+                    <td>NT$${item.product.price * item.quantity}</td>
+                    <td class="discardBtn">
+                        <a href="#" class="material-icons" data-id='${item.id}'>
+                            clear
+                        </a>
+                    </td>
+                  </tr>`
+        });
+        //購物車id不是商品 id
+        cartList.innerHTML=str;//這邊不是+=
+    })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+//刪除單一資料
+cartList.addEventListener('click',function(e){
+    e.preventDefault();//取消預錄型為
+    const cartId =e.target.getAttribute('data-id');
+    //取得data購物車id
+    if(cartId==null){
+        alert('你點到其他東西了')//點到非刪除按鈕時會顯示
+        return;
+    }
+    console.log(cartId);
+    axios.delete(`${url}/api/livejs/v1/customer/${api_path}/carts/${cartId}`)
+    .then(function(response){
+        alert('成功刪除單筆資料');
+        getCartList();
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+});
+
+//刪除購物車全部的內容
+let discardAllBtn=document.querySelector('.discardAllBtn');
+discardAllBtn.addEventListener('click',function(e){
+    e.preventDefault();//A標籤的時候可以用
+    axios.delete(`${url}/api/livejs/v1/customer/${api_path}/carts`)
+        .then(function(response){
+            alert('成功刪除整筆訂單');
+            getCartList();
+        })
+        .catch(function (error) {
+            console.log(error);
+            alert('購物車已經清空，請勿重複點擊')
+        })
+})
